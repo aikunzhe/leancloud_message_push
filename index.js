4b734efb69185775e6d96b14014ed6f27bced171
lunggiyo <978032855@qq.com>
2023-11-13 02:11:16 +0800

    ++

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const http = require('http');
 
-const token = process.env.PUSHPLUS_TOKEN;
-const message = "123";
-const url = `http://www.pushplus.plus/send?token=${token}&content=${message}`;
+const token = process.env.PUSHPLUS_TOKEN; // 使用环境变量
+const content = '123';
+const url = `http://www.pushplus.plus/send?token=${token}&content=${encodeURIComponent(content)}`;
 
-http.get(url, (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-  resp.on('end', () => {
-    console.log('Message sent successfully:', data);
-  });
-}).on("error", (err) => {
-  console.error('Error sending message to PushPlus:', err);
+http.get(url, (res) => {
+    console.log(`状态码: ${res.statusCode}`);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+}).on('error', (error) => {
+    console.error(error);
 });
