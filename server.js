d93ec24a83f6a9dead53be2afa0cfb9373c2e313
lunggiyo <978032855@qq.com>
2023-11-13 05:21:35 +0800

    1

--- server.js
+++ server.js
@@ -1,53 +1,21 @@
-const express = require('express');
-const axios = require('axios');
 const AV = require('leancloud-storage');
+// const axios = require('axios');
+const axios = require('axios/dist/node/axios.cjs');
 
-const app = express();
-const PORT = process.env.LEANCLOUD_APP_PORT || 3001;
+// 初始化 LeanCloud SDK
 
-// LeanCloud 初始化
-AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  serverURL: process.env.LEANCLOUD_API_SERVER,
-});
 
-// 中间件，用于解析请求体中的 JSON 数据
-app.use(express.json());
-
-// 定义路由
-app.get('/sendDataToPushPlus', async (req, res) => {
-  try {
-    // 从 Comment 表获取一条数据
-    const query = new AV.Query('Comment');
-    const comment = await query.first();
-
-    if (!comment) {
-      return res.status(404).json({ error: 'No comment found' });
-    }
-
-    // 获取 PushPlus Token
-    const pushPlusToken = process.env.PUSHPLUS_TOKEN;
-    if (!pushPlusToken) {
-      return res.status(500).json({ error: 'PushPlus Token is not set' });
-    }
-
-    // 构建要发送的数据
-    const sendData = {
-      token: pushPlusToken,
-      title: 'Comment Data',
-      content: `Comment: ${comment.get('content')}`, // 请根据 Comment 表的字段调整
-    };
-
-    // 发送数据到 PushPlus
-    const response = await axios.post('https://pushplus.hxtrip.com/send', sendData);
-
-    // 返回 PushPlus 的响应
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+// 使用查询获取所有 Class Name
+const query = new AV.Query('Comment');
+// let query = new AV.Query('Comment');
+query.find().then(
+  // 获取成功
+  function (results) {
+    const classNames = results.map(result => result.id);
+    console.log('所有 Class Name:', classNames);
+  },
+  // 获取失败
+  function (error) {
+    console.error('获取 Class Name 失败', error);
   }
-});
-
-
+);
