3c7db8f0a9a3613a26d9f137e4da7db3e458b3c2
lunggiyo <978032855@qq.com>
2023-11-13 03:40:43 +0800

    ++

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const AV = require('leancloud-storage');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.LEANCLOUD_APP_PORT || 3000;
 
 // LeanCloud 初始化
 AV.init({
