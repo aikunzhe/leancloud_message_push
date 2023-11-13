1ac727ef7540e39e5f21ac15f1150e538fd4911b
lunggiyo <978032855@qq.com>
2023-11-13 22:34:30 +0800

    ++

--- server.js
+++ server.js
@@ -2,20 +2,63 @@ const AV = require('leancloud-storage');
 // const axios = require('axios');
 const axios = require('axios/dist/node/axios.cjs');
 
-// 初始化 LeanCloud SDK
+// LeanCloud 初始化
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  serverURL: process.env.LEANCLOUD_API_SERVER,
+});
+pushplusToken =  process.env.PUSHPLUS_TOKEN;
+
+async function getCommentMesspushData() {
+
+// 获取 Comment_messpush 表的一条数据
+  const CommentMesspush = AV.Object.extend('Comment_messpush');
+  const query = new AV.Query(CommentMesspush);
+  query.descending('createdAt');  // 按创建时间降序排序
+  query.limit(1);  // 获取一条数据
+  const commentMesspushObject = await query.first();
+  // 打印 Comment_messpush 表的内容
+  // console.log('Comment_messpush data:', commentMesspushObject.toJSON());
+  commentMesspushObjectjson = commentMesspushObject.toJSON();
+  // console.log('Comment_messpush data:', commentMesspushObjectjson);
 
 
-// 使用查询获取所有 Class Name
-const query = new AV.Query('Comment');
-// let query = new AV.Query('Comment');
-query.find().then(
-  // 获取成功
-  function (results) {
-    const classNames = results.map(result => result.id);
-    console.log('所有 Class Name:', classNames);
-  },
-  // 获取失败
-  function (error) {
-    console.error('获取 Class Name 失败', error);
+
+  // 搜索 Comment 表的数据
+  const Comment = AV.Object.extend('Comment');
+  const commentQuery = new AV.Query(Comment);
+
+  commentQuery.greaterThan('insertedAt',  new Date(commentMesspushObjectjson.time));
+  commentQuery.descending('insertedAt'); // 按 insertedAt 降序排序
+  const commentObjects = await commentQuery.find();
+  newTime = 0;
+
+  const length = commentObjects.length;
+  let content = '';
+  if(length){
+    // 将 commentObjects 数组转换为 JSON 字符串
+    content = JSON.stringify(commentObjects.map(commentObject => commentObject.toJSON('\n\n')), null, 2);
+
+    // 提取关键信息并整理格式
+    content = commentObjects.map(commentObject => {
+        return `
+        Nick: ${commentObject.get('nick')},<br>
+        Mail: ${commentObject.get('mail')},<br>
+        Comment: ${commentObject.get('comment')},<br>
+        URL: ${commentObject.get('url')}<br>
+        `;
+    }).join('<br>');
+
   }
-);
+
+  // 发送数据到 PushPlus
+  const pushplusUrl = `https://pushplus.plus/send?token=${pushplusToken}&title=LeanCloud${commentObjects.length}条评论回66复&content=${content}`;
+  await axios.get(pushplusUrl);
+
+}
+
+
+// 执行函数
+getCommentMesspushData();
+
